24ba882ef3749b450fbbc7748d72cabe82c80d43
wocksl <wocksl1998@gmail.com>
2024-10-03 02:35:52 +0900

    fix: workflow

--- server.js
+++ server.js
@@ -58,7 +58,7 @@ const getSecret = async (secretName) => {
 (async () => {
   try {
     // AWS Secrets Manager에서 설정한 비밀 이름
-    const secretName = "bbms/mysql1";  // AWS Secrets Manager에 저장한 비밀 이름
+    const secretName = "bbms/mysql";  // AWS Secrets Manager에 저장한 비밀 이름
     const secret = await getSecret(secretName);
 
     // AWS RDS MySQL 연결 설정
@@ -71,7 +71,7 @@ const getSecret = async (secretName) => {
     
     // IDC MySQL 연결 설정
     var idcDb = mysql.createConnection({
-      host: '10.1.1.100',  // IDC MySQL 서버 DNS
+      host: 'dbsrv.bbidc.internal',  // IDC MySQL 서버 DNS
       user: secret.username,  // 동일한 사용자명 사용 또는 변경
       password: secret.password,  // 동일한 비밀번호 사용 또는 변경
       database: secret.dbname,  // 동일한 데이터베이스 사용 또는 변경
